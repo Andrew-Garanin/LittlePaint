7b5a3d97cd99b08efa5fe4cfa7c42a51dd7e8023
Андрей Гаранин <dkdkdkdkdkdkdkdkdkdkdkdkdkdkdk@yandex.ru>
2021-04-06 17:12:10 +0300

    realized Brezenkhem Algoritm

--- LittlePaint/Form1.cs
+++ LittlePaint/Form1.cs
@@ -27,11 +27,12 @@ namespace LittlePaint
             //DigitalDifferencialAnalizator(20, 20, 200, 600);
         }// Мусор
 
-        private void plot(Point point)
+        private void plot(Point point)// функция рисования пикселя
         {
             Graphics gr = this.CreateGraphics();
             PaintEventArgs paintEvArgs = new PaintEventArgs(gr, this.ClientRectangle);
-            paintEvArgs.Graphics.FillRectangle(br, point.X, point.Y, 2, 2);
+            int pixelSize = 2;
+            paintEvArgs.Graphics.FillRectangle(br, point.X, point.Y, pixelSize, pixelSize);
         }
 
         private void DigitalDifferencialAnalizator(int startX, int startY, int finishX, int finishY)
@@ -44,43 +45,48 @@ namespace LittlePaint
             double deltaErr;// величина приращения
             Point currentPoint = new Point(0, 0);// координаты текущей точки(тут имеется ввиду, что значение текущей точки сдвинуто
                                                  // в начало координат)
-            int octantNum=1;// номер октанта, в котором находится конечная точка          
-            int a=Math.Abs(finishX-startX);
-            if(finishX-startX >= 0 && finishY-startY >=0 && Math.Abs(((double)finishY-startY)/ ((double)finishX-startX))<=1)
+            int octantNum=0;// номер октанта, в котором находится конечная точка
+
+            /* Далее происходит выбор октанта в котором находится конечная точка.
+               В октантах 2,3,6,7 необходимо так же заменить точку a на точку b
+               и вместо обычной погрешности по оординате взять обратную к ней.
+               Это необходимо из-за того, что в этих октантах |x|<=|y|.
+             */
+            if(a >= 0 && b >=0 && Math.Abs((double)b/ (double)a) <=1)
                 octantNum = 1;
-            if (finishX-startX >= 0 && finishY-startY >= 0 && Math.Abs(((double)finishY-startY) / ((double)finishX-startX)) > 1)
+            if (a >= 0 && b >= 0 && Math.Abs((double)b / (double)a) > 1)
             {
                 octantNum = 2;
-                a= Math.Abs(finishY - startY);
+                a= b;
                 err = 1 / err;
             }
-            if (finishX-startX < 0 && finishY-startY >= 0 && Math.Abs(((double)finishY-startY) / ((double)finishX-startX)) > 1)
+            if (a < 0 && b >= 0 && Math.Abs((double)b / (double)a) > 1)
             {
                 octantNum = 3;
-                a = Math.Abs(finishY - startY);
+                a = b;
                 err = 1 / err;
             }
-            if (finishX-startX < 0 && finishY-startY >= 0 && Math.Abs(((double)finishY-startY) / ((double)finishX-startX)) <= 1)
+            if (a < 0 && b >= 0 && Math.Abs((double)b / (double)a) <= 1)
                 octantNum = 4;
-            if (finishX-startX < 0 && finishY-startY < 0 && Math.Abs(((double)finishY-startY) / ((double)finishX-startX)) <= 1)
+            if (a < 0 && b < 0 && Math.Abs((double)b / (double)a) <= 1)
                 octantNum = 5;
-            if (finishX-startX < 0 && finishY-startY < 0 && Math.Abs(((double)finishY-startY) /((double)finishX-startX)) > 1)
+            if (a < 0 && b < 0 && Math.Abs((double)b / (double)a) > 1)
             {
                 octantNum = 6;
-                a = Math.Abs(finishY - startY);
+                a = b;
                 err = 1 / err;
             }
-            if (finishX-startX >= 0 && finishY-startY < 0 && Math.Abs(((double)finishY-startY) / ((double)finishX-startX)) > 1)
+            if (a >= 0 && b < 0 && Math.Abs((double)b / (double)a) > 1)
             {
                 octantNum = 7;
-                a = Math.Abs(finishY - startY);
+                a = b;
                 err = 1 / err;
             }
-            if (finishX-startX >= 0 && finishY-startY < 0 && Math.Abs(((double)finishY-startY) / ((double)finishX-startX)) <= 1)
+            if (a >= 0 && b < 0 && Math.Abs((double)b / (double)a) <= 1)
                 octantNum = 8;
 
             deltaErr = err;
-            while (currentPoint.X < a)
+            while (currentPoint.X < Math.Abs(a))
             {
                 switch (octantNum)
                 {
@@ -152,6 +158,133 @@ namespace LittlePaint
             }
         }
 
+        private void BrezenkhemAlgoritm(int startX, int startY, int finishX, int finishY)
+        {
+            // т.к. у нас начальная точка может не быть нулем, 
+            // мы должны параллельным переносом сдвинуть наш будущий отрезок в начало координат
+            int a = finishX - startX;// координаты конца отрезка(сдвинутые в начало координат)
+            int b = finishY - startY;
+            double err = Math.Abs(2*b) - Math.Abs(a);// текущая погрешность по ординате
+            double deltaErrDiagonal;// величина приращения по диагонали
+            double deltaErrHorizontal;// величина приращения по горизонтали
+            Point currentPoint = new Point(0, 0);// координаты текущей точки(тут имеется ввиду, что значение текущей точки сдвинуто
+                                                 // в начало координат)
+            int octantNum = 0;// номер октанта, в котором находится конечная точка
+
+            /* Далее происходит выбор октанта в котором находится конечная точка.
+               В октантах 2,3,6,7 необходимо так же заменить точку a на точку b
+               и вместо обычной погрешности по ординате поставить "противоположную ей".
+               Это необходимо из-за того, что в этих октантах |x|<=|y|.
+             */
+
+            if (a >= 0 && b >= 0 && Math.Abs((double)b / (double)a) <= 1)
+                octantNum = 1;
+            if (a >= 0 && b >= 0 && Math.Abs((double)b / (double)a) > 1)
+            {
+                octantNum = 2;
+                err = Math.Abs(2 * a) - Math.Abs(b);
+                a = b;
+            }
+            if (a < 0 && b >= 0 && Math.Abs((double)b / (double)a) > 1)
+            {
+                octantNum = 3;
+                err = Math.Abs(2 * a) - Math.Abs(b);
+                a = b;
+            }
+            if (a < 0 && b >= 0 && Math.Abs((double)b / (double)a) <= 1)
+                octantNum = 4;
+            if (a < 0 && b < 0 && Math.Abs((double)b / (double)a) <= 1)
+                octantNum = 5;
+            if (a < 0 && b < 0 && Math.Abs((double)b / (double)a) > 1)
+            {
+                octantNum = 6;
+                err = Math.Abs(2 * a) - Math.Abs(b);
+                a = b;
+            }
+            if (a >= 0 && b < 0 && Math.Abs((double)b / (double)a) > 1)
+            {
+                octantNum = 7;
+                err = Math.Abs(2 * a) - Math.Abs(b);
+                a = b;
+            }
+            if (a >= 0 && b < 0 && Math.Abs((double)b / (double)a) <= 1)
+                octantNum = 8;
+
+            deltaErrDiagonal = err - Math.Abs(a);
+            deltaErrHorizontal = err + Math.Abs(a);
+
+            while (currentPoint.X < Math.Abs(a))
+            {
+                switch (octantNum)
+                {
+                    case 1:
+                        {
+                            Point point = new Point(currentPoint.X + startX, currentPoint.Y + startY);
+                            plot(point);
+                        }
+                        break;
+                    case 2:
+                        {
+                            Point point = new Point(currentPoint.Y + startX, currentPoint.X + startY);
+                            plot(point);
+                        }
+                        break;
+                    case 3:
+                        {
+                            Point point = new Point(-currentPoint.Y + startX, currentPoint.X + startY);
+                            plot(point);
+                        }
+                        break;
+                    case 4:
+                        {
+                            Point point = new Point(-currentPoint.X + startX, currentPoint.Y + startY);
+                            plot(point);
+                        }
+                        break;
+                    case 5:
+                        {
+                            Point point = new Point(-currentPoint.X + startX, -currentPoint.Y + startY);
+                            plot(point);
+                        }
+                        break;
+                    case 6:
+                        {
+                            Point point = new Point(-currentPoint.Y + startX, -currentPoint.X + startY);
+                            plot(point);
+                        }
+                        break;
+                    case 7:
+                        {
+                            Point point = new Point(currentPoint.Y + startX, -currentPoint.X + startY);
+                            plot(point);
+                        }
+                        break;
+                    case 8:
+                        {
+                            Point point = new Point(currentPoint.X + startX, -currentPoint.Y + startY);
+                            plot(point);
+                        }
+                        break;
+                }
+
+                if (err > 0)
+                {
+                    // Диагональное смещение
+                    currentPoint.X += 2;
+                    currentPoint.Y += 2;
+
+                    // т.к. произошло смещение по y на единицу вверх:
+                    err += deltaErrDiagonal;
+                }
+                else
+                {
+                    // Горизонтальное смещение
+                    currentPoint.X += 2;
+                    err += deltaErrHorizontal;
+                }
+            }
+        }
+
         private void colorSelection_Click(object sender, EventArgs e)
         {
             ColorDialog MyDialog = new ColorDialog();
@@ -177,7 +310,8 @@ namespace LittlePaint
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
             Cursor.Clip = new Rectangle();
-            DigitalDifferencialAnalizator(startPoint.X, startPoint.Y, e.X, e.Y);
+            //DigitalDifferencialAnalizator(startPoint.X, startPoint.Y, e.X, e.Y);
+            BrezenkhemAlgoritm(startPoint.X, startPoint.Y, e.X, e.Y);
         }
     }
 }
